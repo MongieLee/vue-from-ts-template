63a06509445664a4c5a3a62902e7664aef4781b2
MongieLee <le30n@outlook.com>
2022-03-30 23:08:27 +0800

    feat(theme): import antd-vue and setting less configuration

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  },
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        path.resolve(__dirname, './src/theme/variables.less')
+      ]
+    }
+  }
+}
